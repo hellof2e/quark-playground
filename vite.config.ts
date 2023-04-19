4684a449cb0981d461fedffdf0bf77e7fdab2f02
Sanqi <sanqi_med@163.com>
2023-04-19 19:56:13 +0800

    feat: build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite';
 import { vitePluginMdToHTML } from 'vite-plugin-md-to-html';
-
+import path from "path";
+const resolve = path.resolve;
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vitePluginMdToHTML()
   ],
+  build: {
+    target: "es2015",
+    outDir: "dist",
+    cssCodeSplit: true,
+    rollupOptions: {
+      input: {
+        vue: resolve(__dirname, "index.html"),
+        preview: resolve(__dirname, "preview.html"),
+      },
+    },
+  },
 })
